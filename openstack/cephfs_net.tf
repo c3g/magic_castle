6aa1650c7e4eb70f905459cece4802b46482cd1b
P-O Quirion <pioliqui@gmail.com>
2024-05-02 13:32:10 -0400

    fix cephfs net security group

--- openstack/cephfs_net.tf
+++ openstack/cephfs_net.tf
@@ -1,4 +1,22 @@
-resource "openstack_compute_interface_attach_v2" "extra_network" {
+resource "openstack_networking_port_v2" "nic_ceph" {
+  for_each           = module.design.instances
+  name               = format("%s-%s-ceph-port", var.cluster_name, each.key)
+  network_id         = "00b327b4-4fb2-4ed8-a7f2-6ff49e3b7e7c"
+  security_group_ids = concat(
+    [
+      openstack_networking_secgroup_v2.global.id
+    ],
+    [
+      for tag, value in openstack_networking_secgroup_v2.external: value.id if contains(each.value.tags, tag)
+    ]
+  )
+  fixed_ip {
+    subnet_id = local.subnet.id
+  }
+}
+
+
+resource "openstack_compute_interface_attach_v2" "ceph_network" {
   for_each    = module.design.instances_to_build
   instance_id = openstack_compute_instance_v2.instances[each.key].id
   network_id  = "00b327b4-4fb2-4ed8-a7f2-6ff49e3b7e7c"
