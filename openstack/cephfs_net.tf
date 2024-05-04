55681e7fb12319970b454dfb5f4279777db484ad
P-O Quirion <pioliqui@gmail.com>
2024-05-03 21:50:09 -0400

    Forgot old block of code

--- openstack/cephfs_net.tf
+++ openstack/cephfs_net.tf
@@ -1,10 +1,3 @@
-resource "openstack_compute_interface_attach_v2" "ceph_network" {
-  for_each    = module.design.instances_to_build
-  instance_id = openstack_compute_instance_v2.instances[each.key].id
-  network_id  = "00b327b4-4fb2-4ed8-a7f2-6ff49e3b7e7c"
-}
-
-
 resource "openstack_networking_port_v2" "nic_ceph" {
   for_each           = module.design.instances
   name               = format("%s-%s-ceph-port", var.cluster_name, each.key)
